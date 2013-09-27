731d7f1d5dafb993fe73ec475ff7d1585d88f4c2
Kat Hoffert <kat.hoffert@gmail.com>
2013-09-27 22:19:01 +0000

    Fixed program so that it should compile and run correctly on all OSes
    git-svn-id: http://fbms.googlecode.com/svn/trunk@45 c375ce47-9a4c-63b9-7dad-5ffa0e8ccef7

--- util/demo/run.sh
+++ util/demo/run.sh
@@ -13,6 +13,8 @@
 # "ant compile". See run.bat for the Windows version.
 cd build
 # Extend the classpath as necessary. Note that Linux separates paths with a
-# colon, different from Windows, which uses a semicolon
-java -classpath ../lib/jnotify-0.94.jar:../lib/sqlite-jdbc-3.7.15-M1.jar:../lib/java-diff-utils-1.3.0.jar:../lib/log4j-1.2.17.jar:. DemoMain
+# colon, different from Windows, which uses a semicolon. Also note that for
+# whatever reason, linux requires that we specify the path to the library
+# (.so) file with -Djava.library.path
+java -classpath ../lib/jnotify-0.94.jar:../lib/sqlite-jdbc-3.7.15-M1.jar:../lib/java-diff-utils-1.3.0.jar:../lib/log4j-1.2.17.jar:. -Djava.library.path=. DemoMain
 cd ../
