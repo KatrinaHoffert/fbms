bfdcd7c0392281363461674f537106abf959b332
Kat Hoffert <kat.hoffert@gmail.com>
2013-09-30 23:07:07 +0000

    Added Eclipse project files
    git-svn-id: http://fbms.googlecode.com/svn/trunk@51 c375ce47-9a4c-63b9-7dad-5ffa0e8ccef7

---
+++ main/src/cmpt370/fbms/Control.java
@@ -0,0 +1,15 @@
+package cmpt370.fbms;
+
+// Making sure libraries work
+import org.apache.log4j.*;
+import difflib.*;
+import net.contentobjects.jnotify.*;
+import java.sql.*;
+
+public class Control
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, world.");
+	}
+}
