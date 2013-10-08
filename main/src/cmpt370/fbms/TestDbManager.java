74ab06a3a8d7af85963094bade8390c2e7a92922
Kat Hoffert <kat.hoffert@gmail.com>
2013-10-08 21:30:24 +0000

    Renaming methods + commenting
    git-svn-id: http://fbms.googlecode.com/svn/trunk@63 c375ce47-9a4c-63b9-7dad-5ffa0e8ccef7

--- main/src/cmpt370/fbms/TestDbManager.java
+++ main/src/cmpt370/fbms/TestDbManager.java
@@ -1,5 +1,6 @@
 package cmpt370.fbms;
 
+// Note the static imports so we can use code like assertTrue() instead of Assert.assertTrue()
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,10 +8,10 @@ import org.junit.Test;
 public class TestDbManager
 {
 	@Test
-	public void testInit()
+	public void testInitReturn()
 	{
-		// DbManager.init() returns true if it is the first run, meaning that
-		// the live directory isn't set (will be null)
+		// DbManager.init() returns true if it is the first run, meaning that the live directory
+		// isn't set (will be null)
 		boolean init = DbManager.init();
 		assertTrue((init && Control.liveDirectory == null)
 				|| (init && Control.liveDirectory != null));
