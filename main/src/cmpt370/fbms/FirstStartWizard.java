918e1fe3a4f6d74b2f30557b7c67233ecfb3588a
Kat Hoffert <kat.hoffert@gmail.com>
2013-10-09 06:30:16 +0000

    Added wizard functionality up until the file choosers portion.
    git-svn-id: http://fbms.googlecode.com/svn/trunk@70 c375ce47-9a4c-63b9-7dad-5ffa0e8ccef7

--- main/src/cmpt370/fbms/FirstStartWizard.java
+++ main/src/cmpt370/fbms/FirstStartWizard.java
@@ -46,30 +46,31 @@ public class FirstStartWizard
 		});
 	}
 
+	// The first panel
 	public static JPanel introPanel()
 	{
-		JPanel introPanel = new JPanel(new BorderLayout());
-		JLabel introLabel = new JLabel("<html>Welcome to the <b>F</b>ile <b>B</b>ackup and "
+		JPanel panel = new JPanel(new BorderLayout());
+		JLabel label = new JLabel("<html>Welcome to the <b>F</b>ile <b>B</b>ackup and "
 				+ "<b>M</b>anagement <b>S</b>ystem, or FBMS. Use the next and previous buttons to "
 				+ "navigate this wizard.</html>");
-		introLabel.setFont(new Font("Sans serif", Font.PLAIN, 14));
-		introPanel.add(introLabel, BorderLayout.NORTH);
-		introPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		label.setFont(new Font("Sans serif", Font.PLAIN, 14));
+		panel.add(label, BorderLayout.NORTH);
+		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
 
 		JPanel buttonPanel = new JPanel(new GridLayout());
 		JButton quitButton = new JButton("Quit");
 		buttonPanel.add(quitButton);
-		JButton introPrevious = new JButton("Previous");
-		introPrevious.setEnabled(false);
-		buttonPanel.add(introPrevious);
-		JButton introNext = new JButton("Next");
-		buttonPanel.add(introNext);
-		introPanel.add(buttonPanel, BorderLayout.SOUTH);
+		JButton prevButton = new JButton("Previous");
+		prevButton.setEnabled(false);
+		buttonPanel.add(prevButton);
+		JButton nextButton = new JButton("Next");
+		buttonPanel.add(nextButton);
+		panel.add(buttonPanel, BorderLayout.SOUTH);
 
 		// Event handler for next button press
-		introNext.addActionListener(new WizardActionListener(1));
+		nextButton.addActionListener(new WizardActionListener(1));
 
-		introPrevious.addActionListener(new WizardActionListener(-1));
+		prevButton.addActionListener(new WizardActionListener(-1));
 
 		// Event handler for quit button press
 		quitButton.addActionListener(new ActionListener()
@@ -81,30 +82,33 @@ public class FirstStartWizard
 			}
 		});
 
-		return introPanel;
+		return panel;
 	}
 
+	// The second panel
 	public static JPanel importPanel()
 	{
-		JPanel introPanel = new JPanel(new BorderLayout());
-		JLabel introLabel = new JLabel("<html>Wanna import?</html>");
-		introLabel.setFont(new Font("Sans serif", Font.PLAIN, 14));
-		introPanel.add(introLabel, BorderLayout.NORTH);
-		introPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		JPanel panel = new JPanel(new BorderLayout());
+		JLabel label = new JLabel("<html>FBMS allows you to recover import old backups. Do "
+				+ "you have an existing backup you wish to import? New users will want to choose "
+				+ "\"no\".</html>");
+		label.setFont(new Font("Sans serif", Font.PLAIN, 14));
+		panel.add(label, BorderLayout.NORTH);
+		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
 
 		JPanel buttonPanel = new JPanel(new GridLayout());
 		JButton quitButton = new JButton("Quit");
 		buttonPanel.add(quitButton);
-		JButton introPrevious = new JButton("Previous");
-		buttonPanel.add(introPrevious);
-		JButton introNext = new JButton("Next");
-		buttonPanel.add(introNext);
-		introPanel.add(buttonPanel, BorderLayout.SOUTH);
+		JButton prevButton = new JButton("No");
+		buttonPanel.add(prevButton);
+		JButton nextButton = new JButton("Yes");
+		buttonPanel.add(nextButton);
+		panel.add(buttonPanel, BorderLayout.SOUTH);
 
 		// Event handler for next button press
-		introNext.addActionListener(new WizardActionListener(1));
+		nextButton.addActionListener(new WizardActionListener(10));
 
-		introPrevious.addActionListener(new WizardActionListener(-1));
+		prevButton.addActionListener(new WizardActionListener(1));
 
 		// Event handler for quit button press
 		quitButton.addActionListener(new ActionListener()
@@ -116,14 +120,93 @@ public class FirstStartWizard
 			}
 		});
 
-		return introPanel;
+		return panel;
+	}
+
+	// The third panel
+	public static JPanel selectDirsPanel()
+	{
+		JPanel panel = new JPanel(new BorderLayout());
+		JLabel label = new JLabel("<html>Select a live directory to monitor and a backup "
+				+ "directory to place your backed up files in.</html>");
+		label.setFont(new Font("Sans serif", Font.PLAIN, 14));
+		panel.add(label, BorderLayout.NORTH);
+		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+		JPanel buttonPanel = new JPanel(new GridLayout());
+		JButton quitButton = new JButton("Quit");
+		buttonPanel.add(quitButton);
+		JButton prevButton = new JButton("Previous");
+		buttonPanel.add(prevButton);
+		JButton nextButton = new JButton("Next");
+		nextButton.setEnabled(false);
+		buttonPanel.add(nextButton);
+		panel.add(buttonPanel, BorderLayout.SOUTH);
+
+		// Event handler for next button press
+		nextButton.addActionListener(new WizardActionListener(1));
+
+		prevButton.addActionListener(new WizardActionListener(-1));
+
+		// Event handler for quit button press
+		quitButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+				System.exit(0);
+			}
+		});
+
+		return panel;
+	}
+
+	// Special case: import old dir
+	public static JPanel selectOldDirPanel()
+	{
+		JPanel panel = new JPanel(new BorderLayout());
+		JLabel label = new JLabel("<html>Specify the directory which held the backup (contains "
+				+ "a file named \".revisions.db\").</html>");
+		label.setFont(new Font("Sans serif", Font.PLAIN, 14));
+		panel.add(label, BorderLayout.NORTH);
+		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+		JPanel buttonPanel = new JPanel(new GridLayout());
+		JButton quitButton = new JButton("Quit");
+		buttonPanel.add(quitButton);
+		JButton prevButton = new JButton("Previous");
+		buttonPanel.add(prevButton);
+		JButton nextButton = new JButton("Next");
+		nextButton.setEnabled(false);
+		buttonPanel.add(nextButton);
+		panel.add(buttonPanel, BorderLayout.SOUTH);
+
+		// Event handler for next button press
+		nextButton.addActionListener(new WizardActionListener(1));
+
+		prevButton.addActionListener(new WizardActionListener(-10));
+
+		// Event handler for quit button press
+		quitButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+				System.exit(0);
+			}
+		});
+
+		return panel;
 	}
 }
 
+// Event handler for next and previous buttons. Takes in an offset and applies that to the current
+// panel to figure out which panel needs to be displayed
 class WizardActionListener implements ActionListener
 {
 	private int offset;
 
+	// Just a way to get the direction forward or back a button takes us
 	public WizardActionListener(int inOffset)
 	{
 		offset = inOffset;
@@ -134,6 +217,8 @@ class WizardActionListener implements ActionListener
 	{
 		if(FirstStartWizard.currentPanel + offset == 1)
 		{
+			// We need to set the content pane with the new panel, then *validate* so it renders
+			// correctly
 			FirstStartWizard.frame.setContentPane(FirstStartWizard.introPanel());
 			FirstStartWizard.frame.validate();
 			FirstStartWizard.currentPanel = 1;
@@ -143,6 +228,18 @@ class WizardActionListener implements ActionListener
 			FirstStartWizard.frame.setContentPane(FirstStartWizard.importPanel());
 			FirstStartWizard.frame.validate();
 			FirstStartWizard.currentPanel = 2;
+		}
+		else if(FirstStartWizard.currentPanel + offset == 3)
+		{
+			FirstStartWizard.frame.setContentPane(FirstStartWizard.selectDirsPanel());
+			FirstStartWizard.frame.validate();
+			FirstStartWizard.currentPanel = 3;
+		}
+		else if(FirstStartWizard.currentPanel + offset == 12)
+		{
+			FirstStartWizard.frame.setContentPane(FirstStartWizard.selectOldDirPanel());
+			FirstStartWizard.frame.validate();
+			FirstStartWizard.currentPanel = 12;
 		}
 	}
 }
