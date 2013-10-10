c8a4113eff74fd92b609aa5cd2b208394643cb5d
Kat Hoffert <kat.hoffert@gmail.com>
2013-10-10 05:55:41 +0000

    Added several visual tests. Some are disabled because they terminate the program. Those can be viewed by commenting out the @Ignore line above the tests.
    git-svn-id: http://fbms.googlecode.com/svn/trunk@89 c375ce47-9a4c-63b9-7dad-5ffa0e8ccef7

--- main/src/cmpt370/fbms/TesterVisual.java
+++ main/src/cmpt370/fbms/TesterVisual.java
@@ -4,6 +4,7 @@ import java.nio.file.Paths;
 import java.util.Date;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -31,5 +32,41 @@ public class TesterVisual
 				System.out.println("\tRevisions size:\t" + file.revisionSizes + " B");
 			}
 		}
+	}
+
+	@Test
+	@Ignore
+	public void errorsFatalError()
+	{
+		Errors.fatalError("She turned me into a newt!");
+	}
+
+	@Test
+	@Ignore
+	public void errorsFatalErrorWithStackTrace()
+	{
+		String someString = null;
+
+		try
+		{
+			someString.charAt(0); // Throws an exception
+		}
+		catch(NullPointerException e)
+		{
+			Errors.fatalError("We dun goofed...", e);
+		}
+	}
+
+	@Test
+	public void errorsNonfatalError()
+	{
+		Errors.nonfatalError("Such error message!<br />&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;"
+				+ "&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;So recovery!<br />&emsp;&emsp;Wow!", "Wow!");
+		try
+		{
+			Thread.sleep(5000); // So the program doesn't instantly terminate
+		}
+		catch(InterruptedException e)
+		{}
 	}
 }
