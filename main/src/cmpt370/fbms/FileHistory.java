2f4b0cbe2b019628ada21404c284045aca55bc50
Kat Hoffert <kat.hoffert@gmail.com>
2013-11-02 22:13:18 +0000

    Renamed class
    git-svn-id: http://fbms.googlecode.com/svn/trunk@180 c375ce47-9a4c-63b9-7dad-5ffa0e8ccef7

--- main/src/cmpt370/fbms/FileHistory.java
+++ main/src/cmpt370/fbms/FileHistory.java
@@ -1,12 +1,33 @@
 package cmpt370.fbms;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileHistory
 {
 	public static Path getRevision(Path file, long timestamp)
 	{
-		return null;
+		// Get the revision in question
+		RevisionInfo revision = DbManager.getRevisionInfo(file, timestamp);
+		Path pathToTempFile = null;
+
+		try
+		{
+			// Create a temporary file for the revision
+			pathToTempFile = Files.createTempFile("revision", ".txt");
+			System.out.println("Temp file : " + pathToTempFile);
+
+			PrintWriter output = new PrintWriter(pathToTempFile.toFile());
+			output.print(revision.diff);
+		}
+		catch(IOException e)
+		{
+			Errors.nonfatalError("Could not create temporary file for revision.", e);
+		}
+
+		return pathToTempFile;
 	}
 
 	public static void storeRevision(Path file, Path diff, long filesize, long delta)
