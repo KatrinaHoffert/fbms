c596cc9f0fc12442f5fecaef14cc5043c828a8f9
Kat Hoffert <kat.hoffert@gmail.com>
2013-11-02 23:40:35 +0000

    Implemented the code that will allow the tray icon to be displayed by FBMS.
    git-svn-id: http://fbms.googlecode.com/svn/trunk@183 c375ce47-9a4c-63b9-7dad-5ffa0e8ccef7

--- main/src/cmpt370/fbms/Control.java
+++ main/src/cmpt370/fbms/Control.java
@@ -47,6 +47,7 @@ public class Control
 	{
 		startup();
 		fileHandler();
+		FrontEnd.createAndShowGUI();
 	}
 
 	/**
--- main/src/cmpt370/fbms/FrontEnd.java
+++ main/src/cmpt370/fbms/FrontEnd.java
@@ -2,7 +2,6 @@ package cmpt370.fbms;
 
 import java.awt.AWTException;
 import java.awt.Image;
-import java.awt.Menu;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
@@ -14,16 +13,12 @@ import java.net.URL;
 import java.nio.file.Paths;
 
 import javax.swing.ImageIcon;
-import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 import cmpt370.fbms.GUI.MainFrame;
 
 public class FrontEnd
 {
-	private static void createAndShowGUI()
+	public static void createAndShowGUI()
 	{
 		// Check the SystemTray support
 		if(!SystemTray.isSupported())
@@ -31,36 +26,34 @@ public class FrontEnd
 			System.out.println("SystemTray is not supported");
 			return;
 		}
-		final PopupMenu popup = new PopupMenu();
-		final TrayIcon trayIcon = new TrayIcon(createImage("res/icon.gif", "tray icon"));
+
+		// Create the icon for the tray
+		final TrayIcon trayIcon = new TrayIcon(createImage("res/icon.gif", "FBMS"));
 		final SystemTray tray = SystemTray.getSystemTray();
 
-		// Create a popup menu components
-		MenuItem aboutItem = new MenuItem("About");
-		Menu displayMenu = new Menu("Display");
+		// Create a popup menu components (when right clicking on icon)
+		final PopupMenu popup = new PopupMenu();
+		MenuItem displayItem = new MenuItem("Display");
 		MenuItem exitItem = new MenuItem("Exit");
-		MenuItem openGUI = new MenuItem("Open");
 
 		// Add components to popup menu
-		popup.add(aboutItem);
-		popup.addSeparator();
-		popup.addSeparator();
-		popup.add(displayMenu);
-		displayMenu.add(openGUI);
+		popup.add(displayItem);
 		popup.add(exitItem);
 
+		// And set that menu to the tray icon
 		trayIcon.setPopupMenu(popup);
 
 		try
 		{
+			// Add to the tray
 			tray.add(trayIcon);
 		}
 		catch(AWTException e)
 		{
-			System.out.println("TrayIcon could not be added.");
-			return;
+			Errors.fatalError("Cannot create system tray icon", e);
 		}
 
+		// Listener for when the tray iucon is clicked
 		trayIcon.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
@@ -70,58 +63,17 @@ public class FrontEnd
 			}
 		});
 
-		aboutItem.addActionListener(new ActionListener()
+		// Listener for when the display item in the right click menu is clicked
+		displayItem.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
 			{
-				JOptionPane.showMessageDialog(null,
-						"This dialog box is run from the About menu item");
+				MainFrame frame = new MainFrame();
+				frame.setVisible(true);
 			}
 		});
 
-
-		ActionListener listener = new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				MenuItem item = (MenuItem) e.getSource();
-				// TrayIcon.MessageType type = null;
-				System.out.println(item.getLabel());
-				if("Error".equals(item.getLabel()))
-				{
-					// type = TrayIcon.MessageType.ERROR;
-					trayIcon.displayMessage("Sun TrayIcon Demo", "This is an error message",
-							TrayIcon.MessageType.ERROR);
-
-				}
-				else if("Warning".equals(item.getLabel()))
-				{
-					// type = TrayIcon.MessageType.WARNING;
-					trayIcon.displayMessage("Sun TrayIcon Demo", "This is a warning message",
-							TrayIcon.MessageType.WARNING);
-
-				}
-				else if("Info".equals(item.getLabel()))
-				{
-					// type = TrayIcon.MessageType.INFO;
-					trayIcon.displayMessage("Sun TrayIcon Demo", "This is an info message",
-							TrayIcon.MessageType.INFO);
-
-				}
-				else if("None".equals(item.getLabel()))
-				{
-					// type = TrayIcon.MessageType.NONE;
-					trayIcon.displayMessage("Sun TrayIcon Demo", "This is an ordinary message",
-							TrayIcon.MessageType.NONE);
-				}
-			}
-		};
-
-		// errorItem.addActionListener(listener);
-		// warningItem.addActionListener(listener);
-		// infoItem.addActionListener(listener);
-		// noneItem.addActionListener(listener);
-
+		// Listener for when the exit item in the right click menu is clicked
 		exitItem.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
@@ -130,30 +82,29 @@ public class FrontEnd
 				System.exit(0);
 			}
 		});
-
-		openGUI.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				MainFrame frame = new MainFrame();
-				frame.setVisible(true);
-			}
-		});
 	}
 
-	protected static Image createImage(String path, String description)
+	/**
+	 * Loads an image from the specified path.
+	 * 
+	 * @param path
+	 * @param description
+	 * @return
+	 */
+	private static Image createImage(String path, String description)
 	{
 		URL imageURL = null;
 		try
 		{
+			// Try to load the image
 			imageURL = Paths.get("").toAbsolutePath().resolve(path).toUri().toURL();
 		}
 		catch(MalformedURLException e)
 		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Errors.fatalError("Error loading image.", e);
 		}
 
+		// Couldn't load the image
 		if(imageURL == null)
 		{
 			System.err.println("Resource not found: " + path);
@@ -164,42 +115,4 @@ public class FrontEnd
 			return (new ImageIcon(imageURL, description)).getImage();
 		}
 	}
-
-	public static void main(String[] args)
-	{
-		/* Use an appropriate Look and Feel */
-		try
-		{
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-			// UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-		}
-		catch(UnsupportedLookAndFeelException ex)
-		{
-			ex.printStackTrace();
-		}
-		catch(IllegalAccessException ex)
-		{
-			ex.printStackTrace();
-		}
-		catch(InstantiationException ex)
-		{
-			ex.printStackTrace();
-		}
-		catch(ClassNotFoundException ex)
-		{
-			ex.printStackTrace();
-		}
-		/* Turn off metal's use of bold fonts */
-		UIManager.put("swing.boldMetal", Boolean.FALSE);
-		// Schedule a job for the event-dispatching thread:
-		// adding TrayIcon.
-		SwingUtilities.invokeLater(new Runnable()
-		{
-			public void run()
-			{
-				createAndShowGUI();
-			}
-		});
-	}
-
 }
