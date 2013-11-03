54284d28f7754019fd594b85f34acfb544309400
Kat Hoffert <kat.hoffert@gmail.com>
2013-11-03 07:39:58 +0000

    Implemented Control.copyTo()
    git-svn-id: http://fbms.googlecode.com/svn/trunk@201 c375ce47-9a4c-63b9-7dad-5ffa0e8ccef7

--- main/src/cmpt370/fbms/Control.java
+++ main/src/cmpt370/fbms/Control.java
@@ -1,20 +1,17 @@
 /*
-	FBMS: File Backup and Management System
-	Copyright (C) 2013 Group 06
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * FBMS: File Backup and Management System Copyright (C) 2013 Group 06
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
 
 package cmpt370.fbms;
 
@@ -322,8 +319,16 @@ public class Control
 		}
 	}
 
+	/**
+	 * Just a wrapper function for the FrontEnd.
+	 * 
+	 * @param sourceFile
+	 *            The path of the file we want to copy.
+	 * @param destFolder
+	 *            The folder we want to copy this file to.
+	 */
 	public static void copyTo(Path sourceFile, Path destFolder)
 	{
-
+		FileOp.copy(sourceFile, destFolder);
 	}
 }
