6bf8f47dcad298adc7d81d041bd138319e9a1110
Kat Hoffert <kat.hoffert@gmail.com>
2013-11-05 01:05:04 +0000

    Converted up and refresh buttons into pretty icons. They're still buttons, but they look like any proper toolbar icon now. Found nice images with compatible licenses for those icons and appropriately credited authors.
    git-svn-id: http://fbms.googlecode.com/svn/trunk@227 c375ce47-9a4c-63b9-7dad-5ffa0e8ccef7

--- main/src/cmpt370/fbms/GUI/MainToolBar.java
+++ main/src/cmpt370/fbms/GUI/MainToolBar.java
@@ -1,27 +1,26 @@
 /*
-	FBMS: File Backup and Management System
-	Copyright (C) 2013 Group 06
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * FBMS: File Backup and Management System Copyright (C) 2013 Group 06
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
 
 package cmpt370.fbms.GUI;
 
 
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
@@ -30,12 +29,29 @@ public class MainToolBar extends JToolBar
 {
 	private static final long serialVersionUID = 1L;
 
-	private JButton upButton = new JButton("up");
-	private JButton refreshButton = new JButton("refresh");
+	// Icons
+	private ImageIcon upIcon = new ImageIcon("res/up.png");
+	private ImageIcon refreshIcon = new ImageIcon("res/refresh.png");
+
+	private JButton upButton = new JButton(upIcon);
+	private JButton refreshButton = new JButton(refreshIcon);
+
 	private JTextField currentDirectory = new JTextField(5);
 
 	public MainToolBar()
 	{
+		// Format those buttons to look natural (no background, border, etc)
+		upButton.setMargin(new Insets(0, 0, 0, 0));
+		upButton.setBorder(null);
+		upButton.setOpaque(false);
+		upButton.setContentAreaFilled(false);
+		upButton.setBorderPainted(false);
+		refreshButton.setMargin(new Insets(0, 0, 0, 0));
+		refreshButton.setBorder(null);
+		refreshButton.setOpaque(false);
+		refreshButton.setContentAreaFilled(false);
+		refreshButton.setBorderPainted(false);
+
 		currentDirectory.setEnabled(false);
 
 		// Refresh button clicked
@@ -60,7 +76,9 @@ public class MainToolBar extends JToolBar
 
 		this.setFloatable(false);
 		this.add(upButton);
+		this.addSeparator();
 		this.add(refreshButton);
+		this.addSeparator();
 		this.add(currentDirectory);
 	}
 }
