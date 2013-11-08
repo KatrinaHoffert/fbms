1c085c4aa1de5f9dc7129fa2f2bc9378c2145649
Da Tao <taoda001@gmail.com>
2013-11-08 03:28:04 +0000

    RevisionInfo.java now implements Comparable for comparing.
    git-svn-id: http://fbms.googlecode.com/svn/trunk@259 c375ce47-9a4c-63b9-7dad-5ffa0e8ccef7

--- main/src/cmpt370/fbms/RevisionInfo.java
+++ main/src/cmpt370/fbms/RevisionInfo.java
@@ -1,28 +1,31 @@
 /*
-	FBMS: File Backup and Management System
-	Copyright (C) 2013 Group 06
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * FBMS: File Backup and Management System Copyright (C) 2013 Group 06
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
 
 package cmpt370.fbms;
 
-public class RevisionInfo
+public class RevisionInfo implements Comparable<RevisionInfo>
 {
 	public long id;
 	public String path;
 	public String diff;
 	public long delta;
 	public long time;
+
+	@Override
+	public int compareTo(RevisionInfo o)
+	{
+		return (int) (o.time - this.time);
+	}
 }
