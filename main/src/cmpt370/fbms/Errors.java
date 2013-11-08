54c10593598e1182100a374edc9880ea50e82e06
Kat Hoffert <kat.hoffert@gmail.com>
2013-11-08 15:31:42 +0000

    Errors.nonFatalError() now logs messages that do not pass a Throwable object.
    git-svn-id: http://fbms.googlecode.com/svn/trunk@275 c375ce47-9a4c-63b9-7dad-5ffa0e8ccef7

--- main/src/cmpt370/fbms/Errors.java
+++ main/src/cmpt370/fbms/Errors.java
@@ -1,21 +1,18 @@
 /*
-	FBMS: File Backup and Management System
-	Copyright (C) 2013 Group 06
+ * FBMS: File Backup and Management System Copyright (C) 2013 Group 06
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-	
 package cmpt370.fbms;
 
 import java.awt.Color;
@@ -101,6 +98,10 @@ public class Errors
 			if(error != null)
 			{
 				Control.logger.error(message, error);
+			}
+			else
+			{
+				Control.logger.error(message);
 			}
 
 			// The frame
