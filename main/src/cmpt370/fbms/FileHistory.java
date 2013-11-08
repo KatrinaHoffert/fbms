97b10e960eda90b94d320613e11c4a50f2d76464
Da Tao <taoda001@gmail.com>
2013-11-08 03:29:56 +0000

    Implemented storeRevision(), partially implemented storeRevision()
    git-svn-id: http://fbms.googlecode.com/svn/trunk@260 c375ce47-9a4c-63b9-7dad-5ffa0e8ccef7

--- main/src/cmpt370/fbms/FileHistory.java
+++ main/src/cmpt370/fbms/FileHistory.java
@@ -1,27 +1,29 @@
 /*
-	FBMS: File Backup and Management System
-	Copyright (C) 2013 Group 06
+ * FBMS: File Backup and Management System Copyright (C) 2013 Group 06
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 package cmpt370.fbms;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class FileHistory
 {
@@ -69,13 +71,65 @@ public class FileHistory
 		return pathToTempFile;
 	}
 
+	/**
+	 * Store the diff file to database and copy the current file to backup folder.
+	 * 
+	 * @param file
+	 *            the actual file.
+	 * @param diff
+	 *            the diff file.
+	 * @param filesize
+	 *            the old file size.
+	 * @param delta
+	 *            change in file size.
+	 */
 	public static void storeRevision(Path file, Path diff, long filesize, long delta)
 	{
-
+		String diffString = null;
+		try
+		{
+			diffString = FileOp.fileToString(diff);
+			FileOp.copy(file, Control.backupDirectory);
+		}
+		catch(IOException e)
+		{
+			Errors.nonfatalError("Could not store " + diff.toAbsolutePath() + " to database.");
+		}
+		DbManager.insertRevision(file, diffString, delta);
 	}
 
 	public static Path obtainRevision(Path file, long timestamp)
 	{
+		// Retrieve data from database
+		List<RevisionInfo> fileRevisionList = DbManager.getRevisionData(file);
+		LinkedList<RevisionInfo> patchList = new LinkedList<>();
+
+		// Add the records we needed to a linked list
+		for(RevisionInfo revisionInfo : fileRevisionList)
+		{
+			if(revisionInfo.time > timestamp)
+			{
+				patchList.add(revisionInfo);
+			}
+		}
+
+		// Sort the linked list in reverse order
+		Collections.sort(patchList);
+		Collections.reverse(patchList);
+
+		// Copy the newest file to temporary folder
+		Path newestFile = FileOp.convertPath(file);
+		FileOp.copy(newestFile, (new File("temp\\")).toPath());
+
+		// Apply diff to the file
+		Path tempPatchFile = (new File("temp\\patch.diff")).toPath();
+		for(RevisionInfo patch : patchList)
+		{
+			// TODO Discuss design of this FileOp.applyDiff()
+			// FileOp.stringToFile(patch.diff, tempPatchFile);
+			// FileOp.applyDiff(sourceFile, afterFile);
+		}
+
 		return null;
 	}
 
